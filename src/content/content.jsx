99ca9d90289ebb7c107ec53a21f85ac5ef6433e3
NiteshAg199 <niteshagarwal141@gmail.com>
2024-12-05 20:04:56 +0530

    Product Key Updated

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -3,7 +3,24 @@ const data={
         "We provide in house training/corporate training in the client site on all latest technologies-AI,Cyber Security,Dev Ops,Selenium,Dev Ops,Mobile Automation,Bigdata,Hadoop, Oracle,SQL,etc.",
         "We focus on contract staffing & recruitment's in the categories-IT Software(Developers,Automations Testers ,scrum masters.Leads and Managers) & IT Enabled Services/BPO."
         ],
-    "Product/Services":[],
+    "Product/Services":[
+            {
+                "cardHeading":"IT consulting-Testing & Development",
+                "cardContent":"We focuses first on the strategic needs of our clients’ businesses to determine the technology capabilities needed to support their long-term goals"
+            },
+            {
+                "cardHeading":"Digital Marketing",
+                "cardContent":"We provide digital marketing campaign by our industry experts to achieve your goal by helping to convert the leads to revenue."
+            },
+            {
+                "cardHeading":"Corporate Training",
+                "cardContent":"We provide in house training/corporate training in the client site on all latest technologies-AI,Cyber Security,Selenium,Dev Ops,Mobile Automation,Bigdata,Hadoop, Oracle,SQL,etc."
+            },
+            {
+                "cardHeading":"Contract Staffing/Placement",
+                "cardContent":"For those companies seeking to access talent and tweak their workforce as per their cyclical needs, Stesin Solutions offers flexible, effective and short/long-term contract staffing solutions." 
+            }
+    ],
     "addresses":[
                     {
                         "location":"Chennai",                                                              
